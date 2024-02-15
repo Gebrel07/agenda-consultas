4bdf16136170e10182def3879f802b656bbf9849
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-02-15 14:03:20 -0300

    renomeado pacote login

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Agenda from "./pages/agenda/Agenda";
 import BuscaHorarios from "./pages/BuscaHorarios/BuscaHorarios";
 import CriarConta from "./pages/criarConta/CriarConta";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+import Login from "./pages/Login/Login";
 import MinhaAgenda from "./pages/MinhaAgenda/MinhaAgenda";
 import CriarProfissional from "./pages/profissional/CriarProfissional";
 import Profissional from "./pages/profissional/Profissional";
