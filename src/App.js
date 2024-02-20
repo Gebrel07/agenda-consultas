a5120ae7fea75d5c1d1830b5731f28465cbe7835
Gabriel <83376188+Gebrel07@users.noreply.github.com>
2024-02-20 14:26:10 -0300

    removidas pags não utilizadas

--- src/App.js
+++ src/App.js
@@ -14,9 +14,6 @@ import Index from "./pages/Index/Index";
 import Login from "./pages/Login/Login";
 import MinhaAgenda from "./pages/MinhaAgenda/MinhaAgenda";
 import MinhaConta from "./pages/MinhaConta/MinhaConta";
-import Agenda from "./pages/agenda/Agenda";
-import CriarProfissional from "./pages/profissional/CriarProfissional";
-import Profissional from "./pages/profissional/Profissional";
 
 // components
 import RouteGuard from "./RouteGuard";
@@ -34,19 +31,13 @@ function App() {
           <div className="app-content">
             <Alert />
             <Routes>
-              <Route
-                path="/profissionais/criar"
-                element={<RouteGuard TargetPage={CriarProfissional} />}
-              />
-              <Route path="/profissionais/:id" element={<RouteGuard TargetPage={Profissional} />} />
-              <Route path="/agenda" element={<RouteGuard TargetPage={Agenda} />} />
               <Route path="/" element={<Index />} />
               <Route path="/criar-conta" element={user ? <Navigate to="/home" /> : <CriarConta />} />
               <Route path="/login" element={user ? <Navigate to="/home" /> : <Login />} />
               <Route path="/home" element={<RouteGuard TargetPage={Home} />} />
               <Route path="/horarios" element={<RouteGuard TargetPage={BuscaHorarios} />} />
-              <Route path="minha-agenda" element={<RouteGuard TargetPage={MinhaAgenda} />} />
-              <Route path="minha-conta" element={<RouteGuard TargetPage={MinhaConta} />} />
+              <Route path="/minha-agenda" element={<RouteGuard TargetPage={MinhaAgenda} />} />
+              <Route path="/minha-conta" element={<RouteGuard TargetPage={MinhaConta} />} />
             </Routes>
           </div>
         </BrowserRouter>
